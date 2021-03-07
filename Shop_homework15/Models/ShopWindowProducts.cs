4d5b1ace6032e4b96ed531817db84ef957f193be
Андрей Федерягин <eniluck@mail.ru>
2021-03-07 09:30:44 +0500

    Changed ShopWindowProduct model

--- Shop_homework15/Models/ShopWindowProducts.cs
+++ Shop_homework15/Models/ShopWindowProducts.cs
@@ -11,15 +11,20 @@ namespace Shop_homework15.Models
     {
         //Идентификатор(порядковый номер, чтобы однозначно идентифицировать товар на витрине)
         public int ID { get; set; }
+
         //Идентификатор витрины(порядковый номер, чтобы однозначно идентифицировать витрину)
         public int ShopWindowID { get; set; }
+
         //Идентификатор товара(порядковый номер, чтобы однозначно идентифицировать товар)
         public int ProductID { get; set; }
+
         //Кол-во товара - в целом можно и сразу считать объем, если посчитаешь это удобным
         public int ProductQuantity { get; set; }
+
         // и сразу считать объем
         public int OverallVolume { get; set; }
+
         // Стоимость товара
-        public decimal MyProperty { get; set; }
+        public decimal Cost { get; set; }
     }
 }
